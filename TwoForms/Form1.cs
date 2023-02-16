26341c918122d123f6e3d990e05024c9ef1be644
jatiinyadav <jatin27yadav@gmail.com>
2023-02-16 12:56:36 +0530

    Add project files.

---
+++ TwoForms/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwoForms
+{
+	public partial class Form1 : Form
+	{
+
+		DataConnectivityDataContext dc;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			dc = new DataConnectivityDataContext();
+			dataGridView1.DataSource = dc.Employees;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Form2 f2 = new Form2();
+			f2.ShowDialog();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+
+			if (dataGridView1.SelectedRows.Count > 0)
+			{
+				Form2 f2 = new Form2();
+
+				f2.idtxt.ReadOnly = true;
+				f2.idtxt.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+				f2.eNametxt.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+				f2.jobtxt.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+				f2.salarytxt.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+				f2.dNametxt.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+
+				f2.ShowDialog();
+
+				dc = new DataConnectivityDataContext();
+				dataGridView1.DataSource = dc.Employees;
+
+			} else
+			{
+				MessageBox.Show("Select a record for updation");
+			}
+
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			if (dataGridView1.SelectedRows.Count > 0)
+			{
+				if (MessageBox.Show("Are You Sure?","Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question)== DialogResult.Yes)
+				{
+					int eno = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[0].Value);
+					Employee obj = dc.Employees.SingleOrDefault(E => E.emo == eno);
+					dc.Employees.DeleteOnSubmit(obj);
+					dc.SubmitChanges();
+					dc = new DataConnectivityDataContext();
+					dataGridView1.DataSource = dc.Employees;
+				}	
+			}
+			else
+			{
+				MessageBox.Show("Select data to delete");
+			}
+		}
+	}
+}
---
+++ TwoForms/Form2.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwoForms
+{
+	public partial class Form2 : Form
+	{
+		DataConnectivityDataContext dc;
+		public Form2()
+		{
+			InitializeComponent();
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			//string connectionString = @"Data Source=SP13884\SQLEXPRESS;Initial Catalog=myDatabase;Integrated Security=True";
+
+			//using (SqlConnection cnn = new SqlConnection(connectionString))
+			//{
+			//	cnn.Open();
+			//	string query = $"INSERT INTO Employee (Ename, Job, Salary, Dname) VALUES ('{eNametxt.Text}', '{jobtxt.Text}', '{salarytxt.Text}', '{dNametxt.Text}')";
+			//	using (SqlCommand cmd = new SqlCommand(query,cnn))
+			//	{
+			//		cmd.ExecuteNonQuery();
+			//	}
+			//}
+
+			dc = new DataConnectivityDataContext();
+
+			if (!idtxt.ReadOnly)
+			{
+				Employee obj = new Employee();
+				obj.emo = int.Parse(idtxt.Text);
+				obj.Ename = eNametxt.Text;
+				obj.Salary = int.Parse(salarytxt.Text);
+				obj.Dname = dNametxt.Text;
+				obj.Job = jobtxt.Text;
+
+
+				dc.Employees.InsertOnSubmit(obj);
+				dc.SubmitChanges();
+
+				MessageBox.Show("Data Inserted");
+			}
+			else
+			{
+				Employee obj = dc.Employees.SingleOrDefault(E => E.emo == int.Parse(idtxt.Text));
+				obj.Ename = eNametxt.Text;
+				obj.Job = jobtxt.Text;
+				obj.Salary = int.Parse(salarytxt.Text);
+				obj.Dname = dNametxt.Text;
+				dc.SubmitChanges();
+				MessageBox.Show("Data Updated");
+				
+			}
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			idtxt.Text = "";
+			eNametxt.Text = "";
+			jobtxt.Text = "";
+			salarytxt.Text = "";
+			dNametxt.Text = "";
+		}
+	}
+}
